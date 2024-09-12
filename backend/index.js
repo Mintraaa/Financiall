0e0caa38b73bccaa4bb0aba886b61238a30573ba
Mintraaa <missmintra2003@gmail.com>
2024-09-12 16:59:27 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const financialRouter = require("./router/financial.router");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const frontend_url = process.env.FRONTEND_URL;
+const corsOption = {
+  origin: frontend_url,
+};
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Financial Tracker API</h1>");
+});
+
+//use router
+app.use("/api/v1/financial", financialRouter);
+
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
+});
